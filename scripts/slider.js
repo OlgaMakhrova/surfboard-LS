80a10e9ade40fbbfd82030be4d8d97af4101f1cb
olga_makhrova <5597177@bk.ru>
2021-07-04 18:37:38 +0300

    hw-3week

---
+++ scripts/slider.js
@@ -0,0 +1,29 @@
+const buttonLeft = document.querySelector('#sliderLeft');
+const buttonRight = document.querySelector('#sliderRight');
+const itemsList = document.querySelector('#slider');
+const items = document.querySelector('.assortment__item');
+
+const step = 940;
+const minRight = 0;
+const maxRight = 940;
+
+let currentRight = 0;
+
+itemsList.style.right = currentRight;
+
+buttonRight.addEventListener("click", e=> {
+  e.preventDefault();
+
+  if(currentRight < maxRight){
+    currentRight += step;
+    itemsList.style.right = `${currentRight}px`;
+  }
+});
+
+buttonLeft.addEventListener('click', e=> {
+  e.preventDefault();
+  if(currentRight > minRight){
+    currentRight -= step;
+    itemsList.style.right = `${currentRight}px`;
+  }
+});
