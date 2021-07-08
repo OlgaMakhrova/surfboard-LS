c653af6526f43f1d6080859f039e52036c7f2ebe
olga_makhrova <5597177@bk.ru>
2021-07-08 20:02:12 +0300

    hw-modal

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,9 +1,27 @@
-const iconMenu = document.querySelector(".menu__icon");
-if(iconMenu){
+const plankBurger = document.querySelector(".burger");
+if(plankBurger){
   const menu = document.querySelector(".menu");
-  iconMenu.addEventListener("click", function (e) {
+  plankBurger.addEventListener("click", function (e) {
     document.body.classList.toggle("_lock");
-    iconMenu.classList.toggle("active");
-    menu.classList.toggle("active");
+    plankBurger.classList.toggle("_active");
+    menu.classList.toggle("_active");
   });
 };
+
+// let burger  = document.querySelector('.burger');
+// let overlay = document.querySelector('.overlay');
+// let body = document.querySelector('body');
+
+// let links = document.querySelectorAll('.menu__link');
+
+// links.forEach(function(element){
+//   element.addEventListener('click' , toggleMenu);
+// })
+
+// function toggleMenu(){
+//   burger.classList.toggle('burger--active');
+//   overlay.classList.toggle('overlay--active');
+//   body.classList.toggle('body--active-menu');
+// }
+
+// burger.addEventListener('click' , toggleMenu);
--- scripts/tabs.js
+++ scripts/tabs.js
@@ -1,19 +1,19 @@
-//  const findBlockByAlias = (alias) => {
-//   return $(".coments__item").filter((ndx, item) =>{
-//     return $(item).attr("data-linked-with") === alias;
-//    });
-//  };
+ const findBlockByAlias = (alias) => {
+  return $(".coments__item").filter((ndx, item) => {
+    return $(item).attr("data-linked-with") === alias;
+   });
+ };
  
 
-// $(".coments__swither-link").click(e =>{
-//   e.preventDefault();
+$(".coments__swither-link").click((e) =>{
+  e.preventDefault();
 
-//   const $this = $(e.currentTarget);
-//   const target =$this.attr(data-open);
-//   const itemToShow = findBlockByAlias(target);
-//   const curItem = $this.closest(".coments__swither-item");
+  const $this = $(e.currentTarget);
+  const target =$this.attr("data-open");
+  const itemToShow = findBlockByAlias(target);
+  const curItem = $this.closest(".coments__swither-item");
 
-//   itemToShow.addClass("active").siblings().removeClass("active");
-//   curItem.addClass("active").siblings().removeClass("active");
+  itemToShow.addClass("active").siblings().removeClass("active");
+  curItem.addClass("active").siblings().removeClass("active");
 
-// });
+});
